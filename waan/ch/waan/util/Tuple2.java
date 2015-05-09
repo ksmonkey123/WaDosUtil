0d070f93940843c659b0b35bd237274eaccc6e15
Andreas Wälchli <andreas.waelchli@me.com>
2015-05-09 21:05:08 +0200

    Created Immutable (Scala-like) 2-Tuple

---
+++ waan/ch/waan/util/Tuple2.java
@@ -0,0 +1,65 @@
+package ch.waan.util;
+
+import java.util.Objects;
+
+/**
+ * Immutable generic 2-Tuple
+ * 
+ * @author Andreas Wälchli
+ * @version 1.1, 2015-05-09
+ *
+ * @param <T1>
+ *            the type of the first element
+ * @param <T2>
+ *            the type or the second element
+ */
+public class Tuple2<T1, T2> {
+
+	/**
+	 * The first element
+	 */
+	public final T1	_1;
+	/**
+	 * The second element
+	 */
+	public final T2	_2;
+
+	/**
+	 * Creates a new tuple instance
+	 * 
+	 * @param _1
+	 *            the first element
+	 * @param _2
+	 *            the second element
+	 */
+	public Tuple2(T1 _1, T2 _2) {
+		this._1 = _1;
+		this._2 = _2;
+	}
+
+	/**
+	 * Returns a tuple with the same elements as this one, but in the reverse
+	 * order
+	 * 
+	 * @return a reversed version of this tuple
+	 */
+	public Tuple2<T2, T1> flip() {
+		return new Tuple2<>(this._2, this._1);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || !(obj instanceof Tuple2))
+			return false;
+		Tuple2<?, ?> other = (Tuple2<?, ?>) obj;
+		return Objects.equals(this._1, other._1)
+				&& Objects.equals(this._2, other._2);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this._1, this._2);
+	}
+}
