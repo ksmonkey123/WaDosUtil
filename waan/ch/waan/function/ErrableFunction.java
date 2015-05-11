a56c24c22144d6516e5f670f84ea209f3630a4aa
Andreas Wälchli <andreas.waelchli@me.com>
2015-05-11 17:43:09 +0200

    more commenting

--- waan/ch/waan/function/ErrableFunction.java
+++ waan/ch/waan/function/ErrableFunction.java
@@ -1,8 +1,29 @@
 package ch.waan.function;
 
+/**
+ * Functional interface for a function (X => Y), that is allowed to throw
+ * exceptions
+ * 
+ * @author Andreas Wälchli
+ * @since 1.1, 2015-05-11
+ *
+ * @param <X>
+ *            the input type
+ * @param <Y>
+ *            the return type
+ */
 @FunctionalInterface
 public interface ErrableFunction<X, Y> {
-	
+
+	/**
+	 * Applies the function to the input value
+	 * 
+	 * @param x
+	 *            the input value
+	 * @return the result
+	 * @throws Exception
+	 *             can be throws if required
+	 */
 	Y apply(X x) throws Exception;
-	
+
 }
--- waan/ch/waan/function/ErrableSupplier.java
+++ waan/ch/waan/function/ErrableSupplier.java
@@ -1,8 +1,24 @@
 package ch.waan.function;
 
+/**
+ * Functional interface for suppliers that may throw exceptions
+ * 
+ * @author Andreas Wälchli
+ * @since 1.1, 2015-05-11
+ *
+ * @param <T>
+ *            return type
+ */
 @FunctionalInterface
 public interface ErrableSupplier<T> {
 
+	/**
+	 * Returns a value
+	 * 
+	 * @return the return value
+	 * @throws Exception
+	 *             can be thrown if required
+	 */
 	T get() throws Exception;
 
 }
